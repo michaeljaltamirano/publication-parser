9e1cd357d1449aabb7d9d5f5cc9fbbb36953df68
mjaltamirano <michael.altamirano@stridehealth.com>
2018-11-18 18:50:13 -0800

    Refactor index to handle NYRB cleanly, others WIP

--- nyrb/index.js
+++ nyrb/index.js
@@ -1,32 +1,16 @@
-const extractor = require("unfluff");
 const fetch = require("node-fetch");
-const request = require("request");
-const https = require("https");
 const fs = require("fs");
 const jsdom = require("jsdom");
 const ENV = require("../env");
+const UTILS = require("../utils");
 const { JSDOM } = jsdom;
+const { fetchContent } = UTILS;
 
 const baseUrl = "https://www.nybooks.com";
 let volumeNumberAndDate;
 
 // const cookie = "wordpress_logged_in_XXX=michaeljaltamirano%40gmail.com%XXX";
-const { nyrbcookie: cookie } = ENV;
-
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let inputUrl;
-
-function fetchContent(url, options) {
-  return fetch(url, options)
-    .then(res => res.text())
-    .catch(err => console.log("err", err));
-}
+const { nyrbCookie: cookie } = ENV;
 
 async function processHrefs(hrefs, options) {
   const dom = new JSDOM(`<!DOCTYPE html>`);
@@ -38,8 +22,6 @@ async function processHrefs(hrefs, options) {
     await fetchContent(href, options)
       .then(result => {
         const articleDom = new JSDOM(result);
-
-        // return;
 
         const article = articleDom.window.document.querySelector(
           "article.article"
@@ -57,7 +39,7 @@ async function processHrefs(hrefs, options) {
 
   fs.writeFile(
     `output/The New York Review of Books - ${volumeNumberAndDate}.html`,
-    dom.window.document.body.innerText,
+    dom.window.document.body.innerHTML,
     err => {
       if (err) throw err;
     }
@@ -66,41 +48,36 @@ async function processHrefs(hrefs, options) {
   console.log("DONE!");
 }
 
-rl.question(
-  "Please enter the full URL for the issue you would like to scrape (e.g. https://www.nybooks.com/issues/2018/12/06/): ",
-  issueUrl => {
-    const options = {
-      credentials: "include",
-      headers: {
-        cookie
-      },
-      referrer: issueUrl,
-      referrerPolicy: "no-referrer-when-downgrade",
-      body: null,
-      method: "GET",
-      mode: "cors"
-    };
+async function nyrbParser(issueUrl) {
+  const options = {
+    credentials: "include",
+    headers: {
+      cookie
+    },
+    referrer: issueUrl,
+    referrerPolicy: "no-referrer-when-downgrade",
+    body: null,
+    method: "GET",
+    mode: "cors"
+  };
 
-    // Get list of articles from the Table of Contents
-    fetchContent(issueUrl, options).then(result => {
-      const dom = new JSDOM(result);
+  // Get list of articles from the Table of Contents
+  fetchContent(issueUrl, options).then(result => {
+    const dom = new JSDOM(result);
+    const tableOfContentsH2s = dom.window.document.querySelectorAll("h2");
+    let hrefs = [];
 
-      const tableOfContentsH2s = dom.window.document.querySelectorAll("h2");
-
-      let hrefs = [];
-
-      tableOfContentsH2s.forEach(h2 => {
-        const a = h2.querySelector("a");
-        if (a) {
-          hrefs.push(a.href);
-        }
-      });
-
-      volumeNumberAndDate = dom.window.document.querySelector("time").innerHTML;
-
-      processHrefs(hrefs, options);
+    tableOfContentsH2s.forEach(h2 => {
+      const a = h2.querySelector("a");
+      if (a) {
+        hrefs.push(a.href);
+      }
     });
 
-    rl.close();
-  }
-);
+    volumeNumberAndDate = dom.window.document.querySelector("time").innerHTML;
+
+    processHrefs(hrefs, options);
+  });
+}
+
+module.exports = nyrbParser;
