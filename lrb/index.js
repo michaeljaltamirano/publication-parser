4381bb1fffb5d2bb6dcc3bb0d82bddb6d854cdb5
mjaltamirano <michael.altamirano@stridehealth.com>
2018-11-18 21:46:34 -0800

    Refactor LRB script for common use, add DOM improvements (less text splitting)

--- lrb/index.js
+++ lrb/index.js
@@ -1,32 +1,15 @@
-const extractor = require("unfluff");
-const fetch = require("node-fetch");
-const request = require("request");
-const https = require("https");
 const fs = require("fs");
 const jsdom = require("jsdom");
-const ENV = require("./env");
+const ENV = require("../env");
+const UTILS = require("../utils");
 const { JSDOM } = jsdom;
+const { fetchContent } = UTILS;
 
 const baseUrl = "https://www.lrb.co.uk";
 let volumeNumberAndDate;
 
 // const cookie = "lrb-source=XXX; lrb-session=XXX; lrb-remember-me=XXX; lrb-segment=XXX";
-const { lrbcookie: cookie } = ENV;
-
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let inputUrl;
-
-function fetchContent(url, options) {
-  return fetch(url, options)
-    .then(res => res.text())
-    .catch(err => console.log("err", err));
-}
+const { lrbCookie: cookie } = ENV;
 
 async function processHrefs(hrefs, options) {
   const dom = new JSDOM(`<!DOCTYPE html>`);
@@ -34,68 +17,44 @@ async function processHrefs(hrefs, options) {
 
   for (const item of hrefs) {
     // Get articles
-    console.log(`Fetching ${baseUrl}${item}...`);
+    console.log(`Fetching: ${baseUrl}${item}`);
     await fetchContent(`${baseUrl}${item}`, options)
       .then(result => {
-        const firstSplit = result.split('<div id="main">');
+        const articleDom = new JSDOM(`<!DOCTYPE html>`);
 
-        if (firstSplit[1].indexOf('<div class="article-body indent"') > 0) {
-          const header = `<hgroup>${
-            firstSplit[1].split("<hgroup>")[1].split("</hgroup>")[0]
-          }</hgroup>`;
+        articleDom.window.document.body.innerHTML = result;
 
-          let books = "";
+        const main = articleDom.window.document.getElementById("main");
+        const article = main.querySelector("div.article-body");
+        const letters = main.querySelector("div.letters");
 
-          // Handles tracking script embedded in article content related
-          // to  book purchase URL
-          if (firstSplit[1].indexOf('<ul class="books"') > 0) {
-            books = `<ul class="books"${
-              firstSplit[1]
-                .split('<ul class="books"')[1]
-                .split('<script type="text/javascript">')[0]
-            }`;
-          }
+        if (article) {
+          // Clean script tags
+          article
+            .querySelectorAll("script")
+            .forEach(script => (script.innerHTML = ""));
 
-          if (firstSplit[1].split('<div id="article-body"')[1] == null) {
-            throw new Error("YOU MUST RESET YOUR COOKIES");
-          }
+          // Clean ads
+          article
+            .querySelectorAll("div.revive-midpage")
+            .forEach(adDiv => (adDiv.innerHTML = ""));
 
-          let article;
-
-          // Handle articles with dropcap and those without, like poems
-          if (firstSplit[1].split('<p class="dropcap">')[1]) {
-            article = `<p class="dropcap">${
-              firstSplit[1]
-                .split('<p class="dropcap">')[1]
-                .split('<div class="print-hide">')[0]
-            }`;
-          } else if (
-            firstSplit[1]
-              .split('<div id="article-body"')[1]
-              .split('<div class="print-hide">')[0]
-          ) {
-            article = `<div id="article-body"${
-              firstSplit[1]
-                .split('<div id="article-body"')[1]
-                .split('<div class="print-hide">')[0]
-            }`;
-          }
-
-          const articleMarkup = `${header}${books}${article}`;
+          // Clean footer
+          article
+            .querySelectorAll("div.print-show")
+            .forEach(printDiv => (printDiv.innerHTML = ""));
 
           return (dom.window.document.body.innerText = `${
             dom.window.document.body.innerText
-          }<div class="NEW-ARTICLE">${articleMarkup}</div><br>End article<br>`);
-        } else if (firstSplit[1].indexOf('<div class="letters">') > 0) {
-          const letters = `<div class="letters">${
-            firstSplit[1]
-              .split('<div class="letters">')[1]
-              .split('<script type="text/javascript">')[0]
-          }`;
-
+          }<div class="NEW-ARTICLE">${
+            article.innerHTML
+          }</div><br>End Article<br>`);
+        } else if (letters) {
           return (dom.window.document.body.innerText = `${
             dom.window.document.body.innerText
-          }<div class="LETTERS"><h1 class="indent">Letters</h1>${letters}</div>`);
+          }<div class="LETTERS"><h1 class="indent">Letters</h1>${
+            letters.innerHTML
+          }</div><br>End Letters<br>`);
         }
 
         // Not sure yet what other cases there are;
@@ -105,7 +64,7 @@ async function processHrefs(hrefs, options) {
   }
 
   fs.writeFile(
-    `output/The London Review of Books - ${volumeNumberAndDate}.html`,
+    `output/lrb/The London Review of Books - ${volumeNumberAndDate}.html`,
     dom.window.document.body.innerText,
     err => {
       if (err) throw err;
@@ -115,51 +74,46 @@ async function processHrefs(hrefs, options) {
   console.log("DONE!");
 }
 
-rl.question(
-  "Please enter the full URL for the issue you would like to scrape (e.g. https://www.lrb.co.uk/v40/n20/contents): ",
-  issueUrl => {
-    const options = {
-      credentials: "include",
-      headers: {
-        cookie: lrbcookie
-      },
-      referrer: issueUrl,
-      referrerPolicy: "no-referrer-when-downgrade",
-      body: null,
-      method: "GET",
-      mode: "cors"
-    };
+async function lrbParser(issueUrl) {
+  const options = {
+    credentials: "include",
+    headers: {
+      cookie
+    },
+    referrer: issueUrl,
+    referrerPolicy: "no-referrer-when-downgrade",
+    body: null,
+    method: "GET",
+    mode: "cors"
+  };
 
-    // Get list of articles
-    fetchContent(issueUrl, options).then(result => {
-      const firstSplit = result.split('<ul class="article-list">');
-      const secondSplit = firstSplit[1].split(
-        '<script type="text/javascript">'
-      );
+  // Get list of articles
+  fetchContent(issueUrl, options).then(result => {
+    const firstSplit = result.split('<ul class="article-list">');
+    const secondSplit = firstSplit[1].split('<script type="text/javascript">');
 
-      const tableOfContentsDom = new JSDOM(secondSplit[0]);
+    const tableOfContentsDom = new JSDOM(secondSplit[0]);
 
-      const getVolumeNumberAndDate = `<p class="issue-link">${
-        result.split('<p class="issue-link">')[1].split("</p>")[0]
-      }</p>`;
+    const getVolumeNumberAndDate = `<p class="issue-link">${
+      result.split('<p class="issue-link">')[1].split("</p>")[0]
+    }</p>`;
 
-      const contents = issueUrl.split(baseUrl)[1];
+    const contents = issueUrl.split(baseUrl)[1];
 
-      volumeNumberAndDate = getVolumeNumberAndDate
-        .split(`<a id="current-issue-link" href="${contents}">`)[1]
-        .split("</a>")[0];
+    volumeNumberAndDate = getVolumeNumberAndDate
+      .split(`<a id="current-issue-link" href="${contents}">`)[1]
+      .split("</a>")[0];
 
-      const links = tableOfContentsDom.window.document.querySelectorAll(
-        "a[class='title']"
-      );
+    const links = tableOfContentsDom.window.document.querySelectorAll(
+      "a[class='title']"
+    );
 
-      const arrayLinks = Array.from(links);
+    const arrayLinks = Array.from(links);
 
-      const hrefs = arrayLinks.map(a => a.href);
+    const hrefs = arrayLinks.map(a => a.href);
 
-      processHrefs(hrefs, options);
-    });
+    processHrefs(hrefs, options);
+  });
+}
 
-    rl.close();
-  }
-);
+module.exports = lrbParser;
