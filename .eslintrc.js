77b030c8f07be67a68accbb4cb59b75e72aae88d
mjaltamirano <michaeljaltamirano@gmail.com>
2020-07-04 11:54:08 -0500

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,12 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
   overrides: [
     {
