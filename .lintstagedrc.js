99601d7a98771345257ef560bb65531c9c395805
michaeljaltamirano <michaeljaltamirano@gmail.com>
2021-03-17 11:07:39 -0700

    Ignore husky, go ahead and format tsconfig.json

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,8 @@
 module.exports = {
-  '*.js': ['yarn prettier --write'],
-  '!(tsconfig).json': ['yarn prettier --write'],
   '*.ts': [
     'yarn prettier --write',
     'yarn eslint --fix',
     () => 'yarn run tsc -p tsconfig.json',
   ],
-  '*.md': ['yarn prettier --write'],
+  '*.{md,js,json,yml}': ['yarn prettier --write'],
 };
